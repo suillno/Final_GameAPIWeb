40f25b69cf1802e37f36887abf670dd710670db2
suillno <shtndlf46@gmail.com>
2025-06-13 15:26:37 +0900

    회원페이지 수정(임시)
    회원페이지 수정(임시)

--- React_GameAPI/game-pickgame-types/src/router/Router.tsx
+++ React_GameAPI/game-pickgame-types/src/router/Router.tsx
@@ -1,26 +1,42 @@
-import { Routes, Route } from "react-router-dom";
+import MainPage from "../layout/MainPage";
+import PublicLayout from "../layout/PublicLayout";
 import Layout from "../layout/Layout";
+
 import GameDetail from "../gameApi/GameDetail";
 import AdminIndex from "../admin/AdminIndex";
-import Library from "../member/Library";
-import MainPage from "../layout/MainPage";
-import Dashboard from "../member/Dashboard";
-import Profile from "../member/Profile";
-import Security from "../member/Security";
-import Wallet from "../member/Wallet";
+
+import Library from "../member/memberDetail/Library";
+import Dashboard from "../member/memberDetail/Dashboard";
+import Profile from "../member/memberDetail/Profile";
+import Security from "../member/memberDetail/Security";
+import Wallet from "../member/memberDetail/Wallet";
+import { Route, Routes } from "react-router-dom";
+import FindId from "../member/memberControl/FindId";
+import FindPw from "../member/memberControl/FindPw";
+import Login from "../member/memberControl/Login";
+import Signup from "../member/memberControl/Signup";
 
 function Router() {
   return (
     <Routes>
+      {/* 메인 레이아웃 */}
       <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
         <Route path="game/:id" element={<GameDetail />} />
-        <Route index element={<MainPage />} /> {/* index: / 경로 */}
         <Route path="admin" element={<AdminIndex />} />
-        <Route path="library" element={<Library />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="security" element={<Security />} />
-        <Route path="wallet" element={<Wallet />} />
+        <Route path="member/library" element={<Library />} />
+        <Route path="member/dashboard" element={<Dashboard />} />
+        <Route path="member/profile" element={<Profile />} />
+        <Route path="member/security" element={<Security />} />
+        <Route path="member/wallet" element={<Wallet />} />
+      </Route>
+
+      {/* 레이아웃 없이 호출 */}
+      <Route path="/member" element={<PublicLayout />}>
+        <Route path="findid" element={<FindId />} />
+        <Route path="findpw" element={<FindPw />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
       </Route>
     </Routes>
   );
